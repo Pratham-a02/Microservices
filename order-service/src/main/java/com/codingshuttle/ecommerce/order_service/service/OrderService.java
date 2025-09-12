8a0d4180b8835bdbac96e40eb21ee0b50406c465
Pratham Pahad <prathampahad012@gmail.com>
2025-09-12 23:01:19 +0530

    added retry and rateLimiter

--- order-service/src/main/java/com/codingshuttle/ecommerce/order_service/service/OrderService.java
+++ order-service/src/main/java/com/codingshuttle/ecommerce/order_service/service/OrderService.java
@@ -2,8 +2,12 @@ package com.codingshuttle.ecommerce.order_service.service;
 
 import com.codingshuttle.ecommerce.order_service.clients.InventoryOpenFeignClient;
 import com.codingshuttle.ecommerce.order_service.dto.OrderRequestDto;
+import com.codingshuttle.ecommerce.order_service.entity.OrderItem;
+import com.codingshuttle.ecommerce.order_service.entity.OrderStatus;
 import com.codingshuttle.ecommerce.order_service.entity.Orders;
 import com.codingshuttle.ecommerce.order_service.repository.OrderRepository;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -33,7 +37,25 @@ public class OrderService {
         return modelMapper.map(order,OrderRequestDto.class);
     }
 
-//    public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
-//        Double totalPrice = inventoryOpenFeignClient.reduceStocks(orderRequestDto);
-//    }
+    @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
+    @RateLimiter(name = "inventoryRateLimiter",fallbackMethod = "createOrderFallback")
+    public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
+        Double totalPrice = inventoryOpenFeignClient.reduceStocks(orderRequestDto);
+        Orders orders = modelMapper.map(orderRequestDto,Orders.class);
+
+        for(OrderItem orderItem:orders.getOrderItem()){
+            orderItem.setOrder(orders);
+        }
+
+        orders.setPrice(totalPrice);
+        orders.setOrderStatus(OrderStatus.CONFIRMED);
+
+        Orders savedOrders = orderRepository.save(orders);
+        return modelMapper.map(savedOrders, OrderRequestDto.class);
+    }
+
+    public OrderRequestDto createOrderFallback(OrderRequestDto orderRequestDto,Throwable throwable) {
+        log.error("Fallback error occurred due to: " + throwable.getMessage());
+        return new OrderRequestDto();
+    }
 }
